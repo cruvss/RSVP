3ebc2af82b7fdf1673c36ce58d29ec9c9fc4e9ba
sachin koirala <sachinkoirala9999@gmail.com>
2026-01-28 16:36:31 +0545

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/speed-reader/',
+  base: '/RSVP/',
   plugins: [
     react(),
     tailwindcss(),
